da8725d938679b30555e686d44191c783a981bda
Aditi Agarwal <aditi2k1@gmail.com>
2023-01-14 01:47:37 +0530

    Create Introduction.js

---
+++ Javascript/Introduction.js
@@ -0,0 +1,29 @@
+console.log("hello world")
+
+function addNum(a,b,c){
+  return a+b+c;
+}
+
+let x= addNum(1,6,3);
+console.log(x);
+
+// nn bb ss u
+
+let a= null
+let b= 23
+let c= true
+let d= BigInt(45683728372)
+let e= "Aditi"
+let f= Symbol("hello")
+let g
+
+console.log(a,b,c,d,e,f,g)
+
+const item = {
+  "harry":true,
+  "aditi": false,
+  "rishi": 34
+}
+
+console.log(item["aditi"])
+
