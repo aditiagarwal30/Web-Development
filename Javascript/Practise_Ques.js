adce042ef977c5265f6b64d62120ddca3f486b44
Aditi Agarwal <aditi2k1@gmail.com>
2023-01-14 01:47:44 +0530

    Create Practise_Ques.js

---
+++ Javascript/Practise_Ques.js
@@ -0,0 +1,19 @@
+let s = "aditi"
+let d = 10
+console.log(s + 10)
+console.log(d + 10)
+
+console.log(typeof (s))
+console.log(typeof (d))
+
+
+const a = {
+  name: "aditi",
+  age: "21"
+}
+
+a['sex'] = "F";
+a['DOB'] = "30oct"
+
+console.log(a)
+
