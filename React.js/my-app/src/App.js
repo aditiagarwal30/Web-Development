5ad494c044c97497d581f37c0f6222089eb74ca2
Aditi Agarwal <aditi2k1@gmail.com>
2023-01-22 17:39:48 +0530

    22/01/23

--- React.js/my-app/src/App.js
+++ React.js/my-app/src/App.js
@@ -1,20 +1,34 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
+import './App.css'
 
 function App() {
-const [counter, setCounter]= useState(0)
-const increment=()=>{
-  setCounter(counter+1);
-}
-const decrement=()=>{
-  setCounter(counter-1);
-}
+  const [counter, setCounter] = useState(0);
 
+  const increment = () => {
+    if(counter==10) return;
+    
+    setCounter(counter+1);
+  }
+
+
+
+  const decrement = () => {
+    if (counter== 0) return;
+    setCounter(counter-1);
+  }
+  
   return (
-    <>
-    <div>{counter}</div>
-    <button onClick={increment}>Increment</button>
-    <button onClick={decrement}>Decrement</button>
-    </>
+    <div className='container'>
+
+      <button onClick={decrement}>-</button>
+      
+      <p className='counter'>
+      {counter}
+      </p>
+
+      <button onClick={increment}>+</button>
+
+    </div>
   )
 }
 
